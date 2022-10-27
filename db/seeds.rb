110f5e61782fefc898fad41b857807d2b9f70dfc
kushagrajain7 <106591189+kushagrajain7@users.noreply.github.com>
2022-10-27 11:38:12 -0500

    DB changes
    Adding DB and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'Interstellar', :rating => 'R',
+    :release_date => '7-Aug-2014'},
+  {:title => 'KGF', :rating => 'R',
+    :release_date => '10-Sep-2018'},
+  {:title => 'RRR', :rating => 'R',
+    :release_date => '8-Oct-2016'},
+  {:title => 'Bahubali-2', :rating => 'R',
+    :release_date => '9-Dec-2015'},
+  {:title => 'spiderman', :rating => 'R',
+    :release_date => '16-July-2010'},
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'}
+  
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
